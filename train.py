c04f550a192d03c3449beee99eba2701e9fb3411
Gopikrishnan Sasikumar <gopikrishnans1996@gmail.com>
2017-06-10 14:47:26 +0530

    Create train.py

--- train.py
+++ train.py
@@ -122,6 +122,31 @@ for iter in range(1,n_iters+1):
        if accuracy<0:
            accuracy=0
        print('accuracy=',round(accuracy),'%','input=',sentence,'actual=',all_categories[out_index[0]],'guess=',all_categories[output_index])
+"""
+accuracy= 0 % input= having a sandwich today? actual= sandwich guess= goodbye
+accuracy= 0 % input= what's for lunch? actual= sandwich guess= sandwich
+accuracy= 15 % input= can you make a sandwich? actual= sandwich guess= sandwich
+accuracy= 10 % input= good day actual= greeting guess= greeting
+accuracy= 36 % input= how is your day? actual= greeting guess= greeting
+accuracy= 24 % input= can you make a sandwich? actual= sandwich guess= sandwich
+accuracy= 49 % input= how is it going today? actual= greeting guess= greeting
+accuracy= 24 % input= good day actual= greeting guess= greeting
+accuracy= 20 % input= have a nice day actual= goodbye guess= goodbye
+accuracy= 60 % input= is there a sandwich shop actual= sandwich guess= sandwich
+accuracy= 46 % input= good morning actual= greeting guess= greeting
+accuracy= 53 % input= can you make a sandwich? actual= sandwich guess= sandwich
+accuracy= 71 % input= how is it going today? actual= greeting guess= greeting
+accuracy= 66 % input= how are you? actual= greeting guess= greeting
+accuracy= 91 % input= find me a sandwich shop actual= sandwich guess= sandwich
+accuracy= 46 % input= hi there hello actual= greeting guess= greeting
+accuracy= 52 % input= hi there hello actual= greeting guess= greeting
+accuracy= 50 % input= have a nice day actual= goodbye guess= goodbye
+accuracy= 79 % input= talk to you soon actual= goodbye guess= goodbye
+accuracy= 78 % input= can you make a sandwich? actual= sandwich guess= sandwich
+
+"""
+    
+    
 #testing       
 def evaluate(line_tensor):
     #output evaluating function
@@ -138,4 +163,11 @@ def predict(sentence):
 predict("good bye")
 predict("where can i get sandwich")
 
+"""
+input= good bye
+intent= greeting
+input= where can i get sandwich
+intent= sandwich
+
+"""
 
