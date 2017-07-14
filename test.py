2c5a0b481daa18ec9baee40f425ce5c307bc3425
GoPi S <gopi@GoPis-MacBook-Air.local>
2017-07-14 20:50:59 +0530

    program for testing

--- test.py
+++ test.py
@@ -1,21 +1,9 @@
 from train import predict
 import random
 import torch
-ann=torch.load('ann.pt')
+ann=torch.load('ann.pt') #importing trained model
 while True:
       k=input("User:")
       intent,top_value,top_index = predict(str(k),ann)
-      print(intent,top_value,top_index)
-      #if intent=="greeting":
-          #print("sakhi:",random.choice(["hello","hi there","greetings","hello it is a pleasure tto meet you"]))
-      #elif intent=="goodbye":
-          #print("sakhi:",random.choice(["see you soon","ok bye","stay safe","have a nice time"]))
-      #elif intent=="bot":
-          #print("sakhi:",random.choice(["my name is sakhi,i am from sango","hi i am sakhi how can i help you?"]))
-      #elif intent=="sango":
-          #print("sakhi",random.choice(["we are sango,we work for women security",
-                                       #"we are a group of engineers called sango. we are trying to reduce crimes using technology","sango,a non profitable organisation working for women safety"]))
-      #elif intent=="alert":
-          #print("sakhi",random.choice(["sending help","triggering panic message","finding nearby safe zone"]))
-      #else:
-          #print("error")
+      print(intent)
+
